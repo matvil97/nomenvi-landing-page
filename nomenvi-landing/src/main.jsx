473969edb7371d52c46b5b2ef108d082b4df4173
matvil97 <84513556+matvil97@users.noreply.github.com>
2026-03-23 20:49:43 +0100

    remove_main_

--- nomenvi-landing/src/main.jsx
+++ nomenvi-landing/src/main.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Contact from "./Contact";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
